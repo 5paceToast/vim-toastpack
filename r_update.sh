049493d6476bd586bf1a48c5968b34c42392054b
Chloe Kudryavtsev <chloe.kudryavtsev@gmail.com>
2016-09-15 13:52:58 -0400

    r_updates will now be called, not dotted

--- r_update.sh
+++ r_update.sh
@@ -1,7 +1,5 @@
 #!/bin/sh
-# meant to be called by dotfiles, but you can just run it yourself
-
-[ -z "$loc" ] && loc="$(dirname $0)" # if being called standalone, don't use cached results
-cd "$loc"
+# meant to be called by dotfiles, but can be used if you're just lazy
+cd "$(dirname $0)" # still haven't decided if using dirname or substitution is better
 git submodule init
 git submodule update
